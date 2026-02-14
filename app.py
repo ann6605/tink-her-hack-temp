871001a78b562c33c2e95516c678fce87b529be3
ann6605 <161941244+ann6605@users.noreply.github.com>
2026-02-14 11:01:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,4 +17,6 @@ def submit_feedback():
     return jsonify({"status":"success", "message":"Feedback saved!"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Read the port from Render
+    app.run(host="0.0.0.0", port=port, debug=True)
+
